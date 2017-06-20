46958777237b420b5cfc0501fb067d0c89001ee8
Alexander Coppock <acoppock@gmail.com>
2017-06-20 13:24:50 +0200

    update stat tools

--- images_maker.R
+++ images_maker.R
@@ -3,56 +3,16 @@ setwd("~/git_projects/acoppock.github.io/")
 library(magick)
 library(magrittr)
 
-
-# For logos
-
-twitter <- image_read("images/twitterLogo.svg", density = 1000)
-github <- image_read("images/gitHubLogo2.svg", density = 1000)
-dataverse <- image_read("images/dataverseLogo.png")
-
-dataverse %>% 
-  image_convert(format = "svg") %>%
-  image_write(path = "images/dataverseLogo.svg")
-
-
-
-orcid <- image_read("images/orcidLogo.png")
-
-orcid %>% 
-  image_convert(format = "svg") %>%
-  image_write(path = "images/orcidLogo.svg")
-
-
-yale <- image_read("images/yaleLogo.png")
-
-yale %>% 
-  image_convert(format = "svg") %>%
-  image_write(path = "images/yaleLogo.svg")
-
-
-
-gscholar <- image_read("images/gscholarLogo.png")
-
-gscholar %>% 
-  image_convert(format = "svg") %>%
-  image_write(path = "images/gscholarLogo.svg")
-
-
-
-twitter
-
-
-
-
-
 # For papers
 files <- list.files(path = "papers/", pattern = ".pdf")
 files <- files[files != "CG_habit.pdf"]
 
-file_names <- gsub(pattern = ".pdf", replacement = "", x = files)
+file_names <- gsub(pattern = ".pdf",
+                   replacement = "",
+                   x = files)
 
-for(i in 1:length(files)){
-    image_read(paste0("papers/", files[i], "[0]"), density = 500) %>%
+for (i in 1:length(files)) {
+  image_read(paste0("papers/", files[i], "[0]"), density = 500) %>%
     image_scale("1000") %>%
     image_crop("1000x1000") %>%
     image_convert(format = "png") %>%
@@ -63,6 +23,24 @@ for(i in 1:length(files)){
 # Fix habit
 image_read("papers/CG_habit_test_Page_01.png") %>%
   image_scale("1000") %>%
-  image_crop("400x400") %>%
+  image_crop("1000x1000") %>%
   image_write("papers/CG_habit.png")
 
+
+# For logos
+
+# image_read("images/dataverseLogo.png") %>%
+#   image_convert(format = "svg") %>%
+#   image_write(path = "images/dataverseLogo.svg")
+# 
+# image_read("images/orcidLogo.png") %>%
+#   image_convert(format = "svg") %>%
+#   image_write(path = "images/orcidLogo.svg")
+# 
+# image_read("images/yaleLogo.png") %>%
+#   image_convert(format = "svg") %>%
+#   image_write(path = "images/yaleLogo.svg")
+# 
+# image_read("images/gscholarLogo.png") %>%
+#   image_convert(format = "svg") %>%
+#   image_write(path = "images/gscholarLogo.svg")
