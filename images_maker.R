528299fc0cbf8845ed7feb42ba38cdd5a24ea801
Alexander Coppock <acoppock@gmail.com>
2020-01-19 20:16:05 -0500

    Update images_maker.R

--- images_maker.R
+++ images_maker.R
@@ -12,14 +12,20 @@ file_names <- gsub(pattern = ".pdf",
                    replacement = "",
                    x = files)
 
-for (i in 1:length(files)) {
-  try(image_read(paste0("papers/", files[i], "[0]"), density = 500) %>%
-    image_scale("1000") %>%
-    image_crop("1000x1000") %>%
-    image_convert(format = "png") %>%
-    image_write(paste0("papers/", file_names[i], ".png")))
-  print(i)
-}
+#for (i in 1:length(files)) {
+file_names
+i <- 39
+
+
+try(image_read(paste0("papers/", files[i], "[0]"), density = 500) %>%
+      image_scale("1000") %>%
+      image_crop("1000x1000") %>%
+      image_convert(format = "png") %>%
+      image_write(paste0("papers/", file_names[i], ".png")))
+
+
+# print(i)
+# }
 
 
 
@@ -38,7 +44,10 @@ image_read("papers/Coppock_shytrump_Page_01.png") %>%
 
 # project pages images ----------------------------------------------------
 
-image_read("~/Dropbox/Work with me/Sensitive Trump/drafts/SPP_production_docs/results_fig.pdf", density = 500) %>%
+image_read(
+  "~/Dropbox/Work with me/Sensitive Trump/drafts/SPP_production_docs/results_fig.pdf",
+  density = 500
+) %>%
   image_convert(format = "jpg") %>%
   image_write("images/Coppock_shytrump_fig.jpg")
 
@@ -50,15 +59,21 @@ image_read("~/Dropbox/Work with me/Sensitive Trump/drafts/SPP_production_docs/re
 # image_read("images/dataverseLogo.png") %>%
 #   image_convert(format = "svg") %>%
 #   image_write(path = "images/dataverseLogo.svg")
-# 
+#
 # image_read("images/orcidLogo.png") %>%
 #   image_convert(format = "svg") %>%
 #   image_write(path = "images/orcidLogo.svg")
-# 
+#
 # image_read("images/yaleLogo.png") %>%
 #   image_convert(format = "svg") %>%
 #   image_write(path = "images/yaleLogo.svg")
-# 
+#
 # image_read("images/gscholarLogo.png") %>%
 #   image_convert(format = "svg") %>%
 #   image_write(path = "images/gscholarLogo.svg")
+
+
+
+
+# argh every time
+# rm -f .git/index.lock
